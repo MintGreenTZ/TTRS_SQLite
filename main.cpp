2991d2aa2d193489df0c415ca77246c250a30337
MintGreen <tz2012@126.com>
2020-06-10 10:45:05 +0800

    query_transfer supposed related to different trains

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ int main() {
     std::string c;
 	int cnt = 0;
     while (getline(std::cin, c) /*&& ++cnt <= 50000*/) {
-    	//std::cout << c << std::endl;
+    	// std::cout << c << std::endl;
         auto t = parser::parse(c);
         if (t.first == "") break;
 
